c8b48b5e308026d45e902209021a53840026906f
heroes-coding <jeremy@heroes.report>
2017-12-31 20:28:28 -0500

    crazy...

--- src/containers/icon_list.js
+++ src/containers/icon_list.js
@@ -8,21 +8,35 @@ class IconList extends Component {
     return nextProps.iconList !== this.props.iconList
   }
   constructor(props) {
+    console.log(props,'constructor for icon list')
     super(props)
+    this.state = {
+      selected: props.iconList.map(x => x.selected)
+    }
     this.updateFilterAndHeroes = this.updateFilterAndHeroes.bind(this)
   }
   updateFilterAndHeroes(id) {
     this.props.updateFilter(id, this.props.updateType)
+    // Updating through redux is not working.  Please let this work...
+    this.setState((prevState,props) => {
+      if (id==='A') {
+        return {selected: prevState.selected.map(x => false)}
+      } else {
+        const newState = {selected:[...prevState.selected]}
+        newState.selected[id] = !prevState.selected[id]
+        return newState
+      }
+    })
     // Somewhat hacky solution to make sure the state is updated first
     setTimeout(() => { this.props.filterHeroes(this.props.store); this.forceUpdate() }, 10)
   }
-  renderIcon(d) {
+  renderIcon(d,index) {
     return (
       <FilterIcon
         id={d.id}
         name={d.name}
         key={d.id}
-        selected ={d.selected}
+        selected ={this.state.selected[d.id]}
         updateFilter={this.updateFilterAndHeroes}
       />
     )
@@ -38,7 +52,7 @@ class IconList extends Component {
           }}
         ><i className="fa fa-undo iconOnButton" aria-hidden="true"></i> {this.props.updateType}:</button>
         <span className="iconLabel"></span>
-        {this.props.iconList.map(d => this.renderIcon(d))}
+        {this.props.iconList.map((d,i) => this.renderIcon(d,i))}
       </form>
     )
   }
