c0691cba567b739e7fc53d331f9ba75ce81427a3
heroes-coding <jeremy@heroes.report>
2018-02-15 22:56:26 -0500

    had to add javascript version for unpack talents... something is wrong with C++ version

--- src/helpers/unpack_talents.js
+++ src/helpers/unpack_talents.js
@@ -27,10 +27,10 @@ function heapFromBytes(response, hero) {
       const replaysPointer = window.Module._decodeTalents(buf,nBuilds)
 
       let o = replaysPointer/4
-      const nTalents = window.Module.HEAPU32[o]
-      const nFull = window.Module.HEAPU32[o+1]
-      const nPartial = window.Module.HEAPU32[o+2]
-      const talentCounts = [3,4,5,6,7,8,9].map(x => window.Module.HEAPU32[o+x])
+      const nTalents = window.Module.HEAP32[o]
+      const nFull = window.Module.HEAP32[o+1]
+      const nPartial = window.Module.HEAP32[o+2]
+      const talentCounts = [3,4,5,6,7,8,9].map(x => window.Module.HEAP32[o+x])
       o = o+10
       const talents = []
       for (let l=0;l<7;l++) {
@@ -40,9 +40,10 @@ function heapFromBytes(response, hero) {
           o += 7
         }
       }
-      const fullBuilds = window.Module.HEAPU32.slice(o,o+nFull*11)
-      const partialBuilds = window.Module.HEAPU32.slice(o+nFull*11,o+nFull*11+nPartial*9)
+      const fullBuilds = window.Module.HEAP32.slice(o,o+nFull*11)
+      const partialBuilds = window.Module.HEAP32.slice(o+nFull*11,o+nFull*11+nPartial*9)
       // console.log(`It took ${Math.round(window.performance.now()*100 - 100*decodeTime)/100} ms to decode talents`)
+      window.timings['Talent decoding C++ for ' + hero] = Math.round(window.performance.now()*100 - 100*unpackTime)/100
       results = {nTalents,nFull,nPartial,talentCounts,talents,fullBuilds,partialBuilds,dataTime, hero}
       window.talentResults = results
     } catch (e) {
@@ -55,6 +56,107 @@ function heapFromBytes(response, hero) {
     resolve(results)
   })
   return promise
+}
+
+function getTalentsWithJavascript(response,hero) {
+  let unpackTime = window.performance.now()
+  const dataTime = new Date(response[0])
+  const talentList = response.slice(1,8)
+  const realTalents = talentList.map(x => x.filter(y => y))
+  const levCounts = talentList.map(x => x.length)
+  const talentCounts = realTalents.map(x => x.length)
+  const nTalents = talentCounts.reduce((x,y) => x+y)
+  let partialBuilds = []
+  let fullBuilds = []
+  const buildKeyDic = [{},{},{},{},{},{},{}]
+  const talents = [0,1,2,3,4,5,6].map(l => realTalents[l].map((t,index) => {
+    buildKeyDic[l][t] = {index, builds:[]}
+    return [t,0,0,0,0,0,0]
+  }))
+  let nFull = 0
+  for (let b=8;b<response.length;b++) {
+    const buildKeys = [0,0,0,0,0,0,0,0,0,0,0]
+    let [ sum, count, wins ] = response[b]
+    let isFullBuild = true
+    for (let t=6;t>=0;t--) {
+      let mult = levCounts[t]
+      let tal = sum%mult
+      sum = Math.floor(sum/mult)
+      if (talentList[t][tal] === null) isFullBuild=false
+      buildKeys[t] = talentList[t][tal]
+    }
+    buildKeys[7] = wins
+    buildKeys[8] = count
+    if (isFullBuild) {
+      buildKeys.slice(0,7).map((k,i) => {
+        buildKeyDic[i][k].builds.push(nFull)
+        let index = buildKeyDic[i][k].index
+        for (let type=1;type<3;type++) {
+          talents[i][index][type*2+1] += wins
+          talents[i][index][type*2+2] += count
+        }
+      })
+      buildKeys[9] = wins*1000
+      buildKeys[10] = count*1000
+      nFull += 1
+      fullBuilds.push(buildKeys)
+    } else {
+      partialBuilds.push(buildKeys.slice(0,9))
+    }
+  }
+  const nPartial = partialBuilds.length
+  const partialMatches = []
+  const partialCounts = []
+
+  for (let p=0;p<nPartial;p++) {
+    let { index: indexKey, builds: potentialMatches } = buildKeyDic[0][partialBuilds[p][0]]
+
+    for (let l=0;l<7;l++) {
+      let talNum = partialBuilds[p][l]
+      if (!talNum) break
+      if (l > 0) indexKey = buildKeyDic[l][talNum].index
+      talents[l][indexKey][3] += partialBuilds[p][7]
+      talents[l][indexKey][4] += partialBuilds[p][8]
+      let nPots = potentialMatches.length
+      let potMatches = []
+      for (let m=0;m<nPots;m++) {
+        let fIndex = potentialMatches[m]
+        if (fullBuilds[fIndex][l] === talNum) potMatches.push(fIndex)
+      }
+      potentialMatches = potMatches
+    }
+    let total = 0
+    let nPots = potentialMatches.length
+    for (let m=0;m<nPots;m++) total += fullBuilds[potentialMatches[m]][8]
+    partialMatches.push(potentialMatches)
+    partialCounts.push(total)
+  }
+  for (let p=0;p<nPartial;p++) {
+    let total = partialCounts[p]
+    let [ wins, count ] = partialBuilds[p].slice(7,9)
+    let potMatches = partialMatches[p]
+    let nPots = potMatches.length
+    for (let m=0;m<nPots;m++) {
+      let bIndex = potMatches[m]
+      let percent = fullBuilds[bIndex][8]/total
+      fullBuilds[bIndex][9] += 1000*wins*percent
+      fullBuilds[bIndex][10] += 1000*count*percent
+    }
+  }
+  for (let b=0;b<nFull;b++) {
+    for (let l=0;l<7;l++) {
+      let indexKey = buildKeyDic[l][fullBuilds[b][l]].index
+      talents[l][indexKey][1] += fullBuilds[b][9]
+      talents[l][indexKey][2] += fullBuilds[b][10]
+    }
+  }
+
+  partialBuilds = new Int32Array([].concat(...partialBuilds))
+  fullBuilds = new Int32Array([].concat(...fullBuilds))
+  window.timings['Talent decoding Javascript for ' + hero] = Math.round(window.performance.now()*100 - 100*unpackTime)/100
+  return {nTalents, nFull, nPartial, talentCounts, talents, fullBuilds, partialBuilds, dataTime, hero}
+
+  // {nTalents,nFull,nPartial,talentCounts,talents,fullBuilds,partialBuilds,dataTime, hero}
 }
 
 export default async function getPackedTalents(hero, prefs) {
@@ -76,7 +178,9 @@ export default async function getPackedTalents(hero, prefs) {
           }
         }
       }
-      resolve(await heapFromBytes(response,hero))
+      // let cppResult = await heapFromBytes(response,hero) // I don't know why this is broken.  Why is it broken?
+      let javascriptResult = getTalentsWithJavascript(response,hero)
+      resolve(javascriptResult)
     }
     binaryReq.send(null)
   })
--- src/webass/cpp/talentsUnpacker.h
+++ src/webass/cpp/talentsUnpacker.h
@@ -4,218 +4,186 @@
 #include <vector>
 #include <map>
 
-void printArray(int build[], int n) {
-    for (int t=0;t<n;t++) {
-      if (t==7) {
-        std::cout << ":::";
-      }
-      std::cout << "[ " << build[t] << " ]";
-    }
-    std::cout << std::endl;
-}
-
-void printBuildKeyDic(std::vector< std::vector< std::vector<int> > > BKD) {
-  for (int l=0;l<BKD.size();l++) {
-    std::cout << "Level " << l << ", length: " << BKD[l].size() << std::endl;
-    for (int t=0;t<BKD[l].size();t++) {
-      std::cout << "Talent " << t << ", length: " << BKD[l][t].size() << std::endl;
-      for (int b=0;b<BKD[l][t].size();b++) {
-        std::cout << "[ " << BKD[l][t][b] << " ]";
-      }
-      std::cout << std::endl;
-    }
-  }
-}
-
-void printTwoDeep(std::vector< std::vector<int> > BKD) {
-  for (int l=0;l<BKD.size();l++) {
-    std::cout << "Level " << l << std::endl;
-    for (int t=0;t<BKD[l].size();t++) {
-      std::cout << "Talent " << t << ": " << BKD[l][t] << std::endl;
-    }
-  }
-}
-
-int32_t * sortTalents (int32_t *buf, std::vector< std::vector<int> > partialBuilds, std::vector< std::vector<int> > fullBuilds, int nPartial, int nFull, std::vector< std::vector<int> > realTalents, int nTalents, int isPlayer) {
-
-  std::vector< std::vector< std::vector<int> > > buildKeyDic;
-  std::vector< std::vector< std::vector<int> > > talentResults;
-
-  buildKeyDic.reserve(14*nFull);
-  talentResults.reserve(200+nFull*6);
-  for (int l=0;l<7;l++) {
-    std::vector< std::vector<int> > levKeys;
-    std::vector< std::vector<int> > levKeys2;
-    buildKeyDic.push_back(levKeys);
-    talentResults.push_back(levKeys2);
-    for (int t=0;t<realTalents[l].size();t++) {
-      std::vector<int> talKeys;
-      std::vector<int> talRes { 0, 0, 0, 0, 0, 0};
-      buildKeyDic[l].push_back(talKeys);
-      talentResults[l].push_back(talRes);
-    }
-  }
-
-  for (int b=0;b<nFull;b++) {
-    for (int l=0;l<7;l++) {
-      for (int t=0;t<realTalents[l].size();t++) {
-        if (realTalents[l][t] == fullBuilds[b][l]) {
-          buildKeyDic[l][t].push_back(b);
-          for (int type=1;type<3;type++) {
-            talentResults[l][t][type*2] +=  fullBuilds[b][7];
-            talentResults[l][t][type*2+1] +=  fullBuilds[b][8];
-          }
-          break;
-        }
-      }
-    }
-  }
-
-  // printBuildKeyDic(buildKeyDic);
-
-
-  std::vector< std::vector<int> > partialMatches;
-  std::vector<int> partialCounts;
-  for (int p=0;p<nPartial;p++) {
-    int indexKey;
-    for (int t=0;t<realTalents[0].size();t++) {
-      // std::cout << "[" << t << ":" << partialBuilds[p][0] << "]";
-      if (partialBuilds[p][0] == realTalents[0][t]) {
-        indexKey = t;
-      }
-    }
-    // std::cout << "**** INDEX KEY ***** (" << indexKey << ")" << std::endl;
-    std::vector<int> potentialMatches = buildKeyDic[0][indexKey];
-    for (int l=0;l<7;l++) {
-      int bKey = partialBuilds[p][l];
-      if (bKey==0) {
-        break;
-      }
-      for (int t=0;t<realTalents[l].size();t++) {
-        if (realTalents[l][t] == bKey) {
-          talentResults[l][t][2] += partialBuilds[p][7];
-          talentResults[l][t][3] += partialBuilds[p][8];
-          break;
-        }
-      }
-      /*
-      if (l==0) {
-        // first level is only for assigning partials above
-        continue;
-      }
-      */
-      std::vector<int> potMatches;
-      int nPots = potentialMatches.size();
-      for (int m=0;m<nPots;m++) {
-        int fIndex = potentialMatches[m];
-        if (fullBuilds[fIndex][l] == bKey) {
-          potMatches.push_back(fIndex);
-        }
-      }
-      potentialMatches = potMatches;
-    }
-    int total = 0;
-    int nPots = potentialMatches.size();
-    for (int m=0;m<nPots;m++) {
-      total += fullBuilds[potentialMatches[m]][8];
-    }
-    partialMatches.push_back(potentialMatches);
-    partialCounts.push_back(total);
-  }
-  // std::cout << nPartial << "N PARTIAL" << std::endl;
-  for (int p=0;p<nPartial;p++) {
-    int total = partialCounts[p];
-    float wins = partialBuilds[p][7];
-    float count = partialBuilds[p][8];
-    std::vector<int> potMatches = partialMatches[p];
-    int nPots = potMatches.size();
-    // std::cout << "p:" << p << ":" << nPots << "[" << wins << "|" << count << "]";
-    for (int m=0;m<nPots;m++) {
-      int bKey = potMatches[m];
-      float percent = isPlayer ? 1 : 1.0*fullBuilds[bKey][8]/total;
-      // std::cout << "[bKey:" << bKey << "] ";
-      /*
-      for (int q=0;q<11;q++) {
-        std::cout << "[" << fullBuilds[bKey][q] << "]";
-      }
-      std::cout << std::endl;
-      */
-      // std::cout << " PERCENT: " << percent;
-      fullBuilds[bKey][9] += int(1000*wins*percent);
-      fullBuilds[bKey][10] += int(1000*count*percent);
-
-    }
-
-  }
-
-  for (int b=0;b<nFull;b++) {
-    for (int l=0;l<7;l++) {
-      for (int t=0;t<realTalents[l].size();t++) {
-        // std::cout << "RT: " << realTalents[l][t] <<  ", FB: " << fullBuilds[b][l];
-        if (realTalents[l][t] == fullBuilds[b][l]) {
-          talentResults[l][t][0] += fullBuilds[b][9];
-          talentResults[l][t][1] += fullBuilds[b][10];
-          break;
-        }
-      }
-    }
-  }
-  
-  // std::cout << std::endl;
-
-  // printBuildKeyDic(talentResults);
-  // printTwoDeep(realTalents);
-  // printBuildKeyDic(buildKeyDic);
-
-  // int32_t returneeBuilds [nFull*11+nPartial*9+nTalents*7+7];
-  int o = 0;
-
-  buf[o++] = nTalents;
-  buf[o++] = nFull;
-  buf[o++] = nPartial;
-
-  // Talent counts
-  for (int l=0;l<7;l++) {
-    buf[o++] = realTalents[l].size();
-  }
-
-  // Talents first
-  for (int l=0;l<7;l++) {
-    for (int t=0;t<realTalents[l].size();t++) {
-      buf[o++] = realTalents[l][t];
-      for (int n=0;n<6;n++) {
-        buf[o++] = talentResults[l][t][n];
-      }
-    }
-  }
-  // Then full builds
-  for (int b=0;b<nFull;b++) {
-    /* std::cout << "[Build:" << b << "]:::";
-    for (int q=0;q<11;q++) {
-      std::cout << "[" << fullBuilds[b][q] << "]";
-    }
-    std::cout << std::endl;
-    */
-    for (int k=0;k<11;k++) {
-      buf[o++] = fullBuilds[b][k];
-    }
-  }
-  // Then partial builds
-  for (int b=0;b<nPartial;b++) {
-    for (int k=0;k<9;k++) {
-      buf[o++] = partialBuilds[b][k];
-    }
-  }
-
-  // std::cout << "FINISHED WITH talents unpacker pointer: " << (uintptr_t)&buf[0]/4 << std::endl;
-  return buf;
-  // auto arrayPtr = &returneeBuilds[0];
-  // return arrayPtr;
-}
 
 extern "C" {
 
+  EMSCRIPTEN_KEEPALIVE
+  int32_t * sortTalents (int32_t *buf, std::vector< std::vector<int> > partialBuilds, std::vector< std::vector<int> > fullBuilds, int nPartial, int nFull, std::vector< std::vector<int> > realTalents, int nTalents, int isPlayer) {
 
+    std::vector< std::vector< std::vector<int> > > buildKeyDic;
+    std::vector< std::vector< std::vector<int> > > talentResults;
+
+    buildKeyDic.reserve(14*nFull);
+    talentResults.reserve(200+nFull*6);
+    for (int l=0;l<7;l++) {
+      std::vector< std::vector<int> > levKeys;
+      std::vector< std::vector<int> > levKeys2;
+      buildKeyDic.push_back(levKeys);
+      talentResults.push_back(levKeys2);
+      for (int t=0;t<realTalents[l].size();t++) {
+        std::vector<int> talKeys;
+        std::vector<int> talRes { 0, 0, 0, 0, 0, 0};
+        buildKeyDic[l].push_back(talKeys);
+        talentResults[l].push_back(talRes);
+      }
+    }
+
+    for (int b=0;b<nFull;b++) {
+      for (int l=0;l<7;l++) {
+        for (int t=0;t<realTalents[l].size();t++) {
+          if (realTalents[l][t] == fullBuilds[b][l]) {
+            buildKeyDic[l][t].push_back(b);
+            for (int type=1;type<3;type++) {
+              talentResults[l][t][type*2] +=  fullBuilds[b][7];
+              talentResults[l][t][type*2+1] +=  fullBuilds[b][8];
+            }
+            break;
+          }
+        }
+      }
+    }
+
+    // printBuildKeyDic(buildKeyDic);
+
+
+    std::vector< std::vector<int> > partialMatches;
+    std::vector<int> partialCounts;
+    for (int p=0;p<nPartial;p++) {
+      int indexKey;
+      for (int t=0;t<realTalents[0].size();t++) {
+        // std::cout << "[" << t << ":" << partialBuilds[p][0] << "]";
+        if (partialBuilds[p][0] == realTalents[0][t]) {
+          indexKey = t;
+        }
+      }
+      // std::cout << "**** INDEX KEY ***** (" << indexKey << ")" << std::endl;
+      std::vector<int> potentialMatches = buildKeyDic[0][indexKey];
+      for (int l=0;l<7;l++) {
+        int bKey = partialBuilds[p][l];
+        if (bKey==0) {
+          break;
+        }
+        for (int t=0;t<realTalents[l].size();t++) {
+          if (realTalents[l][t] == bKey) {
+            talentResults[l][t][2] += partialBuilds[p][7];
+            talentResults[l][t][3] += partialBuilds[p][8];
+            break;
+          }
+        }
+        /*
+        if (l==0) {
+          // first level is only for assigning partials above
+          continue;
+        }
+        */
+        std::vector<int> potMatches;
+        int nPots = potentialMatches.size();
+        for (int m=0;m<nPots;m++) {
+          int fIndex = potentialMatches[m];
+          if (fullBuilds[fIndex][l] == bKey) {
+            potMatches.push_back(fIndex);
+          }
+        }
+        potentialMatches = potMatches;
+      }
+      int total = 0;
+      int nPots = potentialMatches.size();
+      for (int m=0;m<nPots;m++) {
+        total += fullBuilds[potentialMatches[m]][8];
+      }
+      partialMatches.push_back(potentialMatches);
+      partialCounts.push_back(total);
+    }
+    // std::cout << nPartial << "N PARTIAL" << std::endl;
+    for (int p=0;p<nPartial;p++) {
+      int total = partialCounts[p];
+      float wins = partialBuilds[p][7];
+      float count = partialBuilds[p][8];
+      std::vector<int> potMatches = partialMatches[p];
+      int nPots = potMatches.size();
+      // std::cout << "p:" << p << ":" << nPots << "[" << wins << "|" << count << "]";
+      for (int m=0;m<nPots;m++) {
+        int bKey = potMatches[m];
+        float percent = isPlayer ? 1 : 1.0*fullBuilds[bKey][8]/total;
+        // std::cout << "[bKey:" << bKey << "] ";
+        /*
+        for (int q=0;q<11;q++) {
+          std::cout << "[" << fullBuilds[bKey][q] << "]";
+        }
+        std::cout << std::endl;
+        */
+        // std::cout << " PERCENT: " << percent;
+        fullBuilds[bKey][9] += int(1000*wins*percent);
+        fullBuilds[bKey][10] += int(1000*count*percent);
+
+      }
+
+    }
+
+    for (int b=0;b<nFull;b++) {
+      for (int l=0;l<7;l++) {
+        for (int t=0;t<realTalents[l].size();t++) {
+          // std::cout << "RT: " << realTalents[l][t] <<  ", FB: " << fullBuilds[b][l];
+          if (realTalents[l][t] == fullBuilds[b][l]) {
+            talentResults[l][t][0] += fullBuilds[b][9];
+            talentResults[l][t][1] += fullBuilds[b][10];
+            break;
+          }
+        }
+      }
+    }
+
+    // std::cout << std::endl;
+
+    // printBuildKeyDic(talentResults);
+    // printTwoDeep(realTalents);
+    // printBuildKeyDic(buildKeyDic);
+
+    // int32_t returneeBuilds [nFull*11+nPartial*9+nTalents*7+7];
+    int o = 0;
+
+    buf[o++] = nTalents;
+    buf[o++] = nFull;
+    buf[o++] = nPartial;
+
+    // Talent counts
+    for (int l=0;l<7;l++) {
+      buf[o++] = realTalents[l].size();
+    }
+
+    // Talents first
+    for (int l=0;l<7;l++) {
+      for (int t=0;t<realTalents[l].size();t++) {
+        buf[o++] = realTalents[l][t];
+        for (int n=0;n<6;n++) {
+          buf[o++] = talentResults[l][t][n];
+        }
+      }
+    }
+    // Then full builds
+    for (int b=0;b<nFull;b++) {
+      /* std::cout << "[Build:" << b << "]:::";
+      for (int q=0;q<11;q++) {
+        std::cout << "[" << fullBuilds[b][q] << "]";
+      }
+      std::cout << std::endl;
+      */
+      for (int k=0;k<11;k++) {
+        buf[o++] = fullBuilds[b][k];
+      }
+    }
+    // Then partial builds
+    for (int b=0;b<nPartial;b++) {
+      for (int k=0;k<9;k++) {
+        buf[o++] = partialBuilds[b][k];
+      }
+    }
+
+    // std::cout << "FINISHED WITH talents unpacker pointer: " << (uintptr_t)&buf[0]/4 << std::endl;
+    return buf;
+    // auto arrayPtr = &returneeBuilds[0];
+    // return arrayPtr;
+  }
 
   EMSCRIPTEN_KEEPALIVE
   int32_t * getBuilds(
@@ -376,7 +344,7 @@ extern "C" {
   ) {
     /* Takes in the files you generated for all builds server side and deconstructs them into all full and partial builds.  After sending this to the function sortTalents, returns full unselected talent wins / losses for various categories as well as the original partial builds info and the modified (partial build incorporated) full builds. */
     int z = 0;
-    int levCounts [7];
+    std::vector<int> levCounts(7);
     int maxLevCount = 0;
     for (int i=0;i<7;i++) {
       int levCount = buf[z++];
@@ -385,55 +353,36 @@ extern "C" {
       // std::cout << "Level " << i << " count: " << levCounts[i] << std::endl;
     }
     // std::cout << "Max Level count: " << maxLevCount << std::endl;
-    std::vector< std::vector<int> > talentList(7);
-    std::vector< std::vector<int> > realTalents(7);
+    std::vector< std::vector<int> > talentList;
+    std::vector< std::vector<int> > realTalents;
     int nTalents = 0;
     for (int l=0;l<7;l++) {
       std::vector<int> talentTiers(levCounts[l]);
+      std::vector<int> realTalentTiers;
       for (int tier=0;tier<levCounts[l];tier++) {
         int tal = buf[z++];
         talentTiers[tier] = tal;
         if (tal) {
-          realTalents[l].push_back(tal);
+          realTalentTiers.push_back(tal);
           nTalents += 1;
         }
         // std::cout << "Level " << l << " talent tier " << tier << ": " << talentList[l][tier]  << std::endl;
       }
-      talentList[l] = talentTiers;
+      realTalents.push_back(realTalentTiers);
+      talentList.push_back(talentTiers);
     }
 
-
-    std::vector< std::vector<int> > builds;
-    std::vector< std::vector<int> > infos;
-    for (int b=0;b<nBuilds;b++) {
-      std::vector<int> temp{0,0,0,0,0,0,0};
-      builds.push_back(temp);
-      std::vector<int> temp2{0,0};
-      infos.push_back(temp2);
-    }
-
-    // int builds[nBuilds][7];
-    // int infos[nBuilds][2];
-    /*
-    std::vector< std::vector<int> > builds;
-    builds.reserve(7*nBuilds);
-    std::vector< std::vector<float> > info;
-    infos.reserve(2*nBuilds);
-    */
-    std::vector<int> fullIndexes(nBuilds);
-    std::vector<int> partialIndexes(nBuilds);
-
-    // std::vector<int> buildKeyDic[7][maxLevCount];
 
     int nPartial = 0;
     int nFull = 0;
+    std::vector< std::vector<int> > partialBuilds;
+    std::vector< std::vector<int> > fullBuilds;
     for (int b=0;b<nBuilds;b++) {
-      int buildKeys[7];
       int sum = buf[z++];
       int count = buf[z++];
       int wins = buf[z++];
       // std::cout << sum << " (sum), count: " << count << ", wins: " << wins << ", z: " << z << std::endl;
-
+      std::vector<int>temp {0,0,0,0,0,0,0,0,0};
       bool isFullBuild = true;
       for (int t=6;t>=0;t--) {
         int mult = levCounts[t];
@@ -442,58 +391,22 @@ extern "C" {
         if (talentList[t][tal] == 0) {
           isFullBuild=false;
         }
-        builds[b][t] = tal;
-        //buildKeys[t] = tal;
+        temp[t] = talentList[t][tal];
       }
-      /*
-      for (int t=0;t<7;t++) {
-        std::cout << b << " (build), tier: " << t << ", tal: " << buildKeys[t] << ", talName: " << talentList[t][buildKeys[t]] << std::endl;
-      }
-      */
-      // builds[b] = buildKeys;
-      infos[b][0] = wins;
-      infos[b][1] = count;
+
+      temp[7] = wins;
+      temp[8] = count;
 
       if (isFullBuild) {
-          fullIndexes[nFull++] = b;
+          nFull++;
+          temp.push_back(wins*1000);
+          temp.push_back(count*1000);
+          fullBuilds.push_back(temp);
       } else {
-          partialIndexes[nPartial++] = b;
+          nPartial++;
+          partialBuilds.push_back(temp);
       }
 
-    }
-
-
-    std::vector< std::vector<int> > partialBuilds;
-    std::vector< std::vector<int> > fullBuilds;
-    for (int b=0;b<nPartial;b++) {
-      std::vector<int> temp(9);
-      partialBuilds.push_back(temp);
-    }
-    for (int b=0;b<nFull;b++) {
-      std::vector<int> temp(11);
-      fullBuilds.push_back(temp);
-    }
-    // int fullBuilds[nFull][11];
-    // int partialBuilds[nPartial][9];
-    for (int b=0;b<nFull;b++) {
-      int fKey = fullIndexes[b];
-      for (int k=0;k<7;k++) {
-        fullBuilds[b][k] = talentList[k][builds[fKey][k]];
-      }
-      fullBuilds[b][7] = infos[fKey][0];
-      fullBuilds[b][8] = infos[fKey][1];
-      fullBuilds[b][9] = infos[fKey][0]*1000;
-      fullBuilds[b][10] = infos[fKey][1]*1000;
-      // printArray(fullBuilds[b],11);
-    }
-    for (int b=0;b<nPartial;b++) {
-      int fKey = partialIndexes[b];
-      for (int k=0;k<7;k++) {
-        partialBuilds[b][k] = talentList[k][builds[fKey][k]];
-      }
-      partialBuilds[b][7] = infos[fKey][0];
-      partialBuilds[b][8] = infos[fKey][1];
-      // printArray(partialBuilds[b],9);
     }
 
     return sortTalents(buf, partialBuilds,fullBuilds,nPartial,nFull, realTalents, nTalents,0);
